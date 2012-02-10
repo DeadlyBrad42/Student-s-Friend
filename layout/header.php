0c4ae9b92a793785faf95bf98a543baea7eb2f7c
Alex Wardi <aaw5017@gmail.com>
2012-02-10 11:17:47 -0500

    Slight style changes for the header. The links actually point to (blank) pages now

--- layout/header.php
+++ layout/header.php
@@ -3,9 +3,9 @@
           <div id='header'>
             <ul id='nav'>
               <li><a href='#'>Home</a></li>
-              <li><a href='#'>Courses</a></li>
-              <li><a href='#'>User Storage</a></li>
-              <li><a href='#'>Calendar</a></li>
+              <li><a href='courses.php'>Courses</a></li>
+              <li><a href='storage.php'>User Storage</a></li>
+              <li class='last'><a href='calendar.php'>Calendar</a></li>
             </ul>
           <button id='logoutBtn' 
             onclick='FB.logout(function(response){ 
