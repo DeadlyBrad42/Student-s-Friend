228563c79b321ca2c1f26edfe803d9bc3cb5cbe4
jst5066 <jst5066@gmail.com>
2012-04-03 05:59:29 -0700

    Ip change

--- classes/Database.php
+++ classes/Database.php
@@ -10,7 +10,7 @@
     
     public function setConn() {
       // address might become numerical IP if server goes down (take time for DNS to trickle down)
-      $this->conn = new mysqli("troop551.com", "root", "denim", "sf");
+      $this->conn = new mysqli("www.troop551.com", "root", "denim", "sf");
       if(mysqli_connect_errno())
       {
         die("Could not connect to specified database: " . mysqli_connect_error());
